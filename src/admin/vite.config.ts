495d2f00c640f1f0c00a60f91c557c4224b7a243
Aayushhh77 <aayush@probits.com.au>
2025-05-20 17:29:21 +0545

    Build issue fixes

--- src/admin/vite.config.ts
+++ src/admin/vite.config.ts
@@ -1,14 +1,12 @@
 import { mergeConfig, type UserConfig } from 'vite';
-import tailwindcss from 'tailwindcss';
-import autoprefixer from 'autoprefixer';
 
 export default (config: UserConfig) => {
   return mergeConfig(config, {
     css: {
       postcss: {
         plugins: [
-          tailwindcss(),
-          autoprefixer(),
+          require('@tailwindcss/postcss'),
+          require('autoprefixer'),
         ],
       },
     },
